52a861f60caf2c818db03b935fd4e1b542ae200b
Jesse Tane <jesse.tane@gmail.com>
2015-06-18 05:11:22 -0400

    very basic test

---
+++ test.js
@@ -0,0 +1,49 @@
+var tape = require('tape')
+var dbase = require('./')
+var client = dbase.Client()
+var db = null
+
+module.exports = function (server) {
+  client.pipe(server).pipe(client)
+  db = dbase(client)
+}
+
+tape('create data', function (t) {
+  t.plan(3)
+
+  var n = 0
+
+  db.on('value', function (value) {
+    if (n++ === 0) {
+      t.equal(value, undefined)
+    } else {
+      t.equal(value.hello, 'world')
+      db.off('value')
+    }
+  })
+
+  db.update({
+    hello: 'world'
+  }, function (err) {
+    t.error(err)
+  })
+})
+
+tape('remove data', function (t) {
+  t.plan(3)
+
+  var n = 0
+
+  db.on('value', function (value) {
+    if (n++ === 0) {
+      t.equal(value.hello, 'world')
+    } else {
+      t.equal(value, undefined)
+      db.off('value')
+    }
+  })
+
+  db.remove(function (err) {
+    t.error(err)
+  })
+})
