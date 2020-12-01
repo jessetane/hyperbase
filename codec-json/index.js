352ff4e50e67fbc044646a4a9dee6f96b4203146
Jesse Tane <jesse.tane@gmail.com>
2020-12-01 14:17:30 -0500

    add json codec

---
+++ codec-json/index.js
@@ -0,0 +1,30 @@
+import utf8 from 'utf8-transcoder/index.js'
+
+class HyperbaseCodecJson {
+  write (req, cb) {
+    if (!req.id) {
+      req.path.push('_')
+      if (req.data !== null) {
+        req.data = this.serialize(req.data)
+      }
+    }
+    cb(null, req)
+  }
+
+  read (res, cb) {
+    if (res.data) {
+      res.data = this.deserialize(res.data)
+    }
+    cb(null, res)
+  }
+
+  serialize (data) {
+    return utf8.encode(JSON.stringify(data))
+  }
+
+  deserialize (data) {
+    return JSON.parse(utf8.decode(data))
+  }
+}
+
+export default HyperbaseCodecJson
