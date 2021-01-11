4f1581025c43b8ad8d7181a2a6134a924f11f038
Jesse Tane <jesse.tane@gmail.com>
2021-01-11 11:16:49 -0500

    add client, normalize path in cli

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import Hyperbase from 'hyperbase/index.js'
 import HyperbaseTransportUnix from 'hyperbase/transport-unix/index.js'
 import nacl from 'tweetnacl/nacl.js'
 import base64 from 'base64-transcoder/index.js'
@@ -7,11 +8,14 @@ import minimist from 'minimist/index.js'
 
 var argv = minimist(process.argv.slice(2), {
   alias: {
+    d: 'delimiter',
     h: 'help',
     n: 'numeric',
     l: 'limit',
     r: 'reverse',
-    s: 'socket'
+    s: 'socket',
+    w: 'wildcard',
+    o: 'options'
   },
   boolean: [
     'help',
@@ -20,6 +24,19 @@ var argv = minimist(process.argv.slice(2), {
 })
 var args = argv._
 var cmd = args[0]
+var path = args[1]
+var pathDelimiter = argv.delimiter || '/'
+var pathWildcard = argv.wildcard || '*'
+var options = {}
+if (!argv.options) {
+  argv.options = []
+} else if (!Array.isArray(argv.options)) {
+  argv.options = argv.options.split(',')
+}
+argv.options.forEach(pair => {
+  var parts = pair.split(':')
+  options[parts[0]] = parts[1]
+})
 
 if (argv.help) {
   help()
@@ -27,22 +44,28 @@ if (argv.help) {
   connect((err, peer) => {
     if (err) throw err
     if (cmd === 'write') {
-      var key = args[1]
-      var value = args[2]
-      var value = argv.numeric ? parseFloat(value) : value
-      peer.call('write', [[ key, value ]], err => {
+      var data = args[2] || null
+      data = argv.numeric ? parseFloat(data) : data
+      path = Hyperbase.normalizePath(path, { pathDelimiter, pathWildcard })
+      var req = Object.assign({ path, data }, options)
+      peer.call('write', [req], err => {
         if (err) throw err
         process.exit()
       })
     } else if (cmd === 'read') {
-      peer.call('read', args[1], (err, data) => {
+      if (options.decode === 'false') options.decode = false
+      else options.decode = true
+      path = Hyperbase.normalizePath(path, { pathDelimiter, pathWildcard })
+      peer.call('read', path, options, (err, data) => {
         if (err) throw err
         console.log(data)
         process.exit()
       })
     } else if (cmd == 'stream') {
-      var streamId = 'stream.' + (Math.random() + '').slice(2)
-      var opts = { decode: true }
+      if (options.decode === 'false') options.decode = false
+      else options.decode = true
+      var streamId = 'stream.' + Math.random().toString().slice(2)
+      var opts = {}
       opts.limit = argv.limit
       opts.reverse = argv.reverse
       opts.gt = argv.gt
@@ -56,7 +79,8 @@ if (argv.help) {
           process.exit()
         }
       })
-      peer.call('stream', args[1], streamId, opts, err => {
+      path = Hyperbase.normalizePath(path, { pathDelimiter, pathWildcard, allowWild: true })
+      peer.call('stream', path, streamId, Object.assign(opts, options), err => {
         if (err) throw err
       })
     }
@@ -64,15 +88,18 @@ if (argv.help) {
 }
 
 function connect (cb) {
-  var name = 'client.' + (Math.random() + '').slice(2)
+  var name = 'client.' + Math.random().toString().slice(2)
   // console.log(name + ' connecting to ' + argv.socket)
   var unix = new HyperbaseTransportUnix()
   var peer = unix.connect(argv.socket)
   peer.database =  { name }
-  peer.addEventListener('ready', peer.auth)
-  peer.addEventListener('auth', evt => {
+  peer.addEventListener('connect', peer.auth)
+  peer.addEventListener('shouldauth', evt => {
     if (peer.authState !== true) cb(peer.authState)
     else cb(null, peer)
+  })
+  peer.addEventListener('error', evt => {
+    throw evt.detail
   })
 }
 
--- client.js
+++ client.js
@@ -1,115 +1,12 @@
 import HyperbaseTransportUnix from 'hyperbase/transport-unix/index.js'
 import CustomEvent from 'xevents/custom-event.js'
 
-function random () {
-  return (Math.random() + '').slice(2)
-}
-
 function Client (opts) {
-  var name = opts.name || random()
+  var name = opts.name || Math.random().toString().slice(2)
   var unix = new HyperbaseTransportUnix()
   var peer = unix.connect(opts.unixSocket)
   peer.database = { name }
   peer.addEventListener('ready', peer.auth)
-  peer.write = function (batch) {
-    return new Promise((res, rej) => {
-      this.call('write', batch, err => {
-        if (err) rej(err)
-        else res()
-      })
-    })
-  }
-  peer.read = function (path) {
-    return new Promise((res, rej) => {
-      this.call('read', path, { decode: true }, (err, r) => {
-        if (err) rej(err)
-        else res(r)
-      })
-    })
-  }
-  peer.stream = function (path, opts) {
-    var s = new EventTarget()
-    var id = random()
-    opts.decode = true
-    this.call('stream', path, id, opts, err => {
-      if (err) return s.dispatchEvent(new CustomEvent('error', { detail: err }))
-      var timeout = setTimeout(() => {
-        this.setInterface(id, null)
-        s.dispatchEvent(new CustomEvent('error', { detail: new Error('timed out') }))
-      }, opts.timeout || this.timeout || 1000)
-      this.setInterface(id, {
-        data: entry => {
-          s.dispatchEvent(new CustomEvent('data', { detail: entry }))
-        },
-        end: () => {
-          clearTimeout(timeout)
-          this.setInterface(id, null)
-          s.dispatchEvent(new Event('end'))
-        }
-      })
-    })
-    return s
-  }
-  peer.page = function (path, opts) {
-    return new Promise((res, rej) => {
-      var id = random()
-      var items = []
-      opts.decode = true
-      this.call('stream', path, id, opts, err => {
-        if (err) return rej(err)
-        var timeout = setTimeout(() => {
-          this.setInterface(id, null)
-          rej(new Error('timed out'))
-        }, opts.timeout || this.timeout || 1000)
-        this.setInterface(id, {
-          data: item => items.push(item),
-          end: () => {
-            clearTimeout(timeout)
-            this.setInterface(id, null)
-            res(items)
-          }
-        })
-      })
-    })
-  }
-  peer.clear = function (path, opts) {
-    return new Promise((res, rej) => {
-      var id = random()
-      var i = 0
-      var n = 0
-      var ended = false
-      this.call('stream', path, id, opts, err => {
-        if (err) return rej(err)
-        var timeout = setTimeout(() => {
-          timeout = null
-          this.setInterface(id, null)
-          rej(new Error('timed out'))
-        }, opts.timeout || this.timeout || 1000)
-        this.setInterface(id, {
-          data: item => {
-            i++
-            n++
-            this.write(item.path, null).then(() => {
-              if (timeout === null) return
-              if (--n > 0) return
-              if (ended) res(i)
-            }).catch(err => {
-              if (timeout === null) return
-              clearTimeout(timeout)
-              timeout = null
-              this.setInterface(id, null)
-              rej(err)
-            })
-          },
-          end: () => {
-            clearTimeout(timeout)
-            this.setInterface(id, null)
-            ended = true
-          }
-        })
-      })
-    })
-  }
   return peer
 }
 
