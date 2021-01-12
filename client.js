5052867a25a327c55a89adf986decd93ed31915f
Jesse Tane <jesse.tane@gmail.com>
2021-01-12 10:33:10 -0500

    client: use connect event

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 import HyperbaseTransportUnix from 'hyperbase/transport-unix/index.js'
 import CustomEvent from 'xevents/custom-event.js'
 
-function Client (opts) {
+function Client (opts = {}) {
   var name = opts.name || Math.random().toString().slice(2)
   var unix = new HyperbaseTransportUnix()
   var peer = unix.connect(opts.unixSocket)
   peer.database = { name }
-  peer.addEventListener('ready', peer.auth)
+  peer.addEventListener('connect', peer.auth)
   return peer
 }
 
